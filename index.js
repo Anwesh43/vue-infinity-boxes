9208a7e062051bbb3afc784edf951eeb51ac312e
Anwesh43 <anweshthecool0@gmail.com>
2019-08-07 15:11:59 +0530

    adding State class

---
+++ index.js
@@ -0,0 +1,26 @@
+const scGap = 0.1
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
